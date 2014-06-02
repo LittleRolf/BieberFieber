cb16f7b5fca9b2c735e5ce6a65c51c43bb347df9
Belogron <ole.wehrmeyer@ewetel.net>
2014-06-02 12:29:33 +0200

    Teller and Bieber

---
+++ src/de/LittleRolf/BieberFieber/Bieber.java
@@ -0,0 +1,10 @@
+package de.LittleRolf.BieberFieber;
+
+public class Bieber {
+
+	public enum Size {BIG, SMALL};
+	
+	public Bieber(String name, Bieber.Size s) {
+		
+	}
+}
---
+++ src/de/LittleRolf/BieberFieber/Teller.java
@@ -0,0 +1,10 @@
+package de.LittleRolf.BieberFieber;
+
+public class Teller {
+
+	public enum Size {BIG, SMALL};
+	
+	public Teller(String name, Bieber.Size s) {
+		
+	}
+}
